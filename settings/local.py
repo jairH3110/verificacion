0f0395b7e2165f6715092c5fe852fb09817c5386
jairH3110 <chiefeon3110@gmail.com>
2023-02-18 19:31:34 -0600

    querys

--- settings/local.py
+++ settings/local.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'links',
+    
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema',
+}
